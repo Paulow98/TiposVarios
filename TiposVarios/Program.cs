0a8e6e9d122b144a9b7f38767e301772a8b3380d
Paulow98 <paulow98@hotmail.com>
2018-02-15 10:43:39 +0000

    Add and Write variables

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,6 +10,22 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            double d = 1312;
+            uint ui = 12013U;
+            int i = 100;
+            long l = 123L;
+            ulong ul = 3UL;
+            decimal de = 3.13m;
+            Console.WriteLine(d);
+            Console.WriteLine(ui);
+            Console.WriteLine(i);
+            Console.WriteLine(l);
+            Console.WriteLine(ul);
+            Console.WriteLine(de);
+
+
         }
     }
 }
+
+
